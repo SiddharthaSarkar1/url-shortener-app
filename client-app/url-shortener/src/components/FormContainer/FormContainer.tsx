28ed959b364463b3877481094bcf20d8e561fa2e
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-16 01:22:13 +0530

    add basic components

---
+++ client-app/url-shortener/src/components/FormContainer/FormContainer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IFormContainerProps {
+}
+
+const FormContainer: React.FunctionComponent<IFormContainerProps> = () => {
+  return (
+    <div className="container mx-auto p-2">
+        <div className=""></div>
+    </div>
+  );
+};
+
+export default FormContainer;
